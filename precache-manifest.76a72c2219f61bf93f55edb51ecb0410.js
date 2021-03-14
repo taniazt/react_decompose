8c36b08709f73a8a1ba120c4d2afaffe809ed178
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-03-14 23:28:39 +0200

    Updates

---
+++ precache-manifest.76a72c2219f61bf93f55edb51ecb0410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2186a2d5900a6a4f8d5a82a3fb4119d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff40d2b9222cdfe82819",
+    "url": "./static/css/main.48283339.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff40d2b9222cdfe82819",
+    "url": "./static/js/main.c1c04efc.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
